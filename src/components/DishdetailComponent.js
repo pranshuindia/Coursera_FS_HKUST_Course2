be53ecd7815b72ecae1de1015756ba5dc77452b1
pranshuindia <37049162+pranshuindia@users.noreply.github.com>
2020-11-18 15:34:50 +0530

    Presentational and Container Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -14,7 +14,7 @@ class Dishdetail extends Component {
             const commentsList = comments.map((comment)=>{
                 return (
                     <li key={comment.id}>
-                        {comment.comment} <br/> <br/> -- {comment.author} , {comment.date} <br/> <br/>
+                        {comment.comment} <br/> <br/> -- {comment.author} , {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))} <br/> <br/>
                     </li>
                 )
             });
@@ -36,19 +36,21 @@ class Dishdetail extends Component {
     render() {
         if (this.props.dish != null) {
             return (
-                <div className={"row"}>
-                    <div className="col-12 col-md-5 m-1">
-                        <Card>
-                            <CardImg width="100%" src={this.props.dish.image} alt={this.props.dish.name}/>
-                            <CardBody>
-                                <CardTitle>{this.props.dish.name}</CardTitle>
-                                <CardText>{this.props.dish.description}</CardText>
-                            </CardBody>
-                        </Card>
-                    </div>
-                    <div className={"col-12 col-md-5 m-1"}>
-                        <h4>Comments</h4>
-                        {this.renderComment(this.props.dish.comments)}
+                <div className={"container"}>
+                    <div className={"row"}>
+                        <div className="col-12 col-md-5 m-1">
+                            <Card>
+                                <CardImg width="100%" src={this.props.dish.image} alt={this.props.dish.name}/>
+                                <CardBody>
+                                    <CardTitle>{this.props.dish.name}</CardTitle>
+                                    <CardText>{this.props.dish.description}</CardText>
+                                </CardBody>
+                            </Card>
+                        </div>
+                        <div className={"col-12 col-md-5 m-1"}>
+                            <h4>Comments</h4>
+                            {this.renderComment(this.props.dish.comments)}
+                        </div>
                     </div>
                 </div>
             )
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,14 +1,9 @@
 import React, {Component} from "react";
 import {Card, CardImg, CardImgOverlay, CardTitle} from "reactstrap";
-import Dishdetail from "./DishdetailComponent";
 
 class Menu extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            selectedDish : null
-        };
-
         console.log("Menu Constructor Invoked");
     }
 
@@ -16,17 +11,12 @@ class Menu extends Component {
         console.log("Menu DidMount Invoked");
     }
 
-    onDishSelect(dish) {
-        this.setState({
-            selectedDish : dish
-        })
-    }
 
     render() {
         const menu = this.props.dishes.map((dish)=>{
             return (
                 <div key={dish.id} className="col-12 col-md-5 m-1">
-                    <Card onClick={()=>this.onDishSelect(dish)}>
+                    <Card onClick={() => this.props.onClick(dish.id)}>
                         <CardImg width="100%" src={dish.image} alt={dish.name} />
                         <CardImgOverlay>
                             <CardTitle>{dish.name}</CardTitle>
@@ -41,7 +31,6 @@ class Menu extends Component {
                 <div className="row">
                         {menu};
                 </div>
-                <Dishdetail dish={this.state.selectedDish} />
             </div>
         );
     }
