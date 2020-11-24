989e847b4d8704352621422c31694ce464e3a554
pranshuindia <37049162+pranshuindia@users.noreply.github.com>
2020-11-24 23:04:09 +0530

    React Animations

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -12,7 +12,8 @@ import About from "./AboutComponent";
 import {withRouter} from "react-router-dom";
 import {connect} from 'react-redux';
 import {postComment, fetchComments, fetchDishes, fetchPromos} from "../redux/ActionCreators";
-import { actions } from 'react-redux-form';
+import {actions} from 'react-redux-form';
+import {TransitionGroup, CSSTransition} from 'react-transition-group';
 
 const mapStateToProps = state => {
     return {
@@ -25,8 +26,12 @@ const mapStateToProps = state => {
 
 const mapDisptachToProps = (dispatch) => ({
     postComment: (dishId, rating, author, comment) => dispatch(postComment(dishId, rating, author, comment)),
-    fetchDishes : () => {dispatch(fetchDishes())},
-    resetFeedbackForm: () => { dispatch(actions.reset('feedback'))},
+    fetchDishes: () => {
+        dispatch(fetchDishes())
+    },
+    resetFeedbackForm: () => {
+        dispatch(actions.reset('feedback'))
+    },
     fetchComments: () => dispatch(fetchComments()),
     fetchPromos: () => dispatch(fetchPromos())
 });
@@ -36,6 +41,7 @@ class Main extends Component {
     constructor(props) {
         super(props);
     }
+
     componentDidMount() {
         this.props.fetchDishes();
         this.props.fetchComments();
@@ -63,12 +69,13 @@ class Main extends Component {
 
         const DishWithId = ({match}) => {
             return (
-                <DishDetail dish={this.props.dishes.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
-                            isLoading={this.props.dishes.isLoading}
-                            errMess={this.props.dishes.errMess}
-                            comments={this.props.comments.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
-                            commentsErrMess={this.props.comments.errMess}
-                            postComment={this.props.postComment}
+                <DishDetail
+                    dish={this.props.dishes.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
+                    isLoading={this.props.dishes.isLoading}
+                    errMess={this.props.dishes.errMess}
+                    comments={this.props.comments.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))}
+                    commentsErrMess={this.props.comments.errMess}
+                    postComment={this.props.postComment}
                 />
             );
         };
@@ -82,14 +89,19 @@ class Main extends Component {
         return (
             <div>
                 <Header/>
-                <Switch>
-                    <Route path='/home' component={HomePage}/>
-                    <Route path='/aboutus' component={AboutUs}/>
-                    <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes}/>}/>
-                    <Route path='/menu/:dishId' component={DishWithId}/>
-                    <Route exact path='/contactus' component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
-                    <Redirect to="/home"/>
-                </Switch>
+                <TransitionGroup>
+                    <CSSTransition key={this.props.location.key} classNames="page" timeout={300}>
+                        <Switch location={this.props.location}>
+                            <Route path='/home' component={HomePage}/>
+                            <Route exact path='/aboutus' component={() => <About leaders={this.props.leaders}/>}/>} />
+                            <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes}/>}/>
+                            <Route path='/menu/:dishId' component={DishWithId}/>
+                            <Route exact path='/contactus'
+                                   component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm}/>}/>
+                            <Redirect to="/home"/>
+                        </Switch>
+                    </CSSTransition>
+                </TransitionGroup>
                 <Footer/>
             </div>
         );
